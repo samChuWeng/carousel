345f2039fc2ea141a5dd51ca5e698bc70833e717
chuweng <sam.chuweng@gmail.com>
2022-12-01 16:12:29 +0800

    clean code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
-///// Section-1 CSS-Slider /////    
+///// Carousel CSS-Slider /////    
   // Auto Switching Images for CSS-Slider
-  function bannerSwitcher() {
-    next = $('.sec-1-input').filter(':checked').next('.sec-1-input');
-    console.log(next);
-    if (next.length) next.prop('checked', true);
-    else $('.sec-1-input').first().prop('checked', true);
-  }
-
-  // var bannerTimer = setInterval(bannerSwitcher, 5000);
-
-  $('nav .controls label').click(function() {
-    clearInterval(bannerTimer);
-    bannerTimer = setInterval(bannerSwitcher, 5000)
-  });
-
   function carouselSwitcher() {
     now = $('.carousel-item').filter('.active');
     now.attr('class', 'carousel-item');
@@ -23,14 +9,3 @@
   }
 
   var bannerTimer = setInterval(carouselSwitcher, 5000);
-
-///// Anchor Smooth Scroll /////
-//   $('.main-menu a, .learn-more-button a').click(function(e){
-    
-//     e.preventDefault();
-        
-//     var target = $(this).attr('href');
-        
-//     $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
-//     return false;
-//   });
